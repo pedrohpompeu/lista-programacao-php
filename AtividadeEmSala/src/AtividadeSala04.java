26f5c37d0b8a0c154de745891df3f6f9273bb633
PedroPompeu <pedropompeu1909@gmail.com>
2025-08-20 19:41:09 -0300

    Atividade em sala 04

---
+++ AtividadeEmSala/src/AtividadeSala04.java
@@ -0,0 +1,10 @@
+public class AtividadeSala04 {
+    public static void main(String[] args){
+        int x = 0;
+        System.out.println("Valor de X +=1:"+ (x +=1));
+        System.out.println("Valor de X ++:"+ ( x++));
+        System.out.println("Valor de X -=2:"+ (x -=2));
+        System.out.println("Valor de X:"+ x);
+        System.out.println("Valor de X --:"+ (x--));
+    }
+}
