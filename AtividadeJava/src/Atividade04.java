1006277bec91a5bf4cbf9bc6f2f02c943a981313
PedroPompeu <pedropompeu1909@gmail.com>
2025-08-28 19:47:19 -0300

    Atividade04

---
+++ AtividadeJava/src/Atividade04.java
@@ -0,0 +1,7 @@
+public class Atividade04 {
+    public static void main(String[] args) {
+        final double pi = 3.1415;
+        double raio = 4.34;
+        System.out.println("PI="+pi+","+"Raio="+raio);
+    }
+}
