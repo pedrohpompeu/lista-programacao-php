a4b616c4325853e2710cadb676923de7aa36b3ac
PedroPompeu <pedropompeu1909@gmail.com>
2025-08-28 19:17:55 -0300

    Atividade01

---
+++ AtividadeJava/src/Atividade01.java
@@ -0,0 +1,8 @@
+public class Atividade01 {
+    public static void main(String[] args) {
+        String nome = "Pedro";
+        String cidade = "São Gonçalo do Rio Abaixo";
+        final String pais = "Brasil";
+        System.out.println("Olá eu sou "+ nome +" moro em "+ cidade +","+pais);
+    }
+}
