b2fa45f620b42f94b14d75474649ac1125ade2bf
PedroPompeu <pedropompeu1909@gmail.com>
2025-08-28 19:53:52 -0300

    Atividade05

---
+++ AtividadeJava/src/Atividade05.java
@@ -0,0 +1,7 @@
+public class Atividade05 {
+    public static void main(String[] args) {
+        String curso = "PROGRAMAÇÃO COM JAVA E C# .NET";
+        int cargaHoraria = 360;
+        System.out.println("Curso:"+curso+", Carga Horaria:"+cargaHoraria);
+    }
+}
