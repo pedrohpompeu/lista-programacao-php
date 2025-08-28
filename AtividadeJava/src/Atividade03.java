1aed0f8780b821850aef17791298c3323022d3f2
PedroPompeu <pedropompeu1909@gmail.com>
2025-08-28 19:42:47 -0300

    Atividade03

---
+++ AtividadeJava/src/Atividade03.java
@@ -0,0 +1,12 @@
+public class Atividade03 {
+    public static void main(String[] args) {
+        int qtdAlunos = 5;
+        double mediaTurma = (4+7+5.5+6+7.5)/5;
+        boolean aprovada = (mediaTurma>=6);
+        String professor = "Victor";
+        System.out.println("qtdAlunos:"+qtdAlunos);
+        System.out.println("Média da turma:"+mediaTurma);
+        System.out.println("Turma foi Aprovada:"+aprovada);
+        System.out.println("provessor:"+professor);
+    }
+}
