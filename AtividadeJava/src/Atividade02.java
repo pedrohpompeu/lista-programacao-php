b24579800260020917a38e63871da621ff38ad17
PedroPompeu <pedropompeu1909@gmail.com>
2025-08-28 19:23:21 -0300

    Atividade02

---
+++ AtividadeJava/src/Atividade02.java
@@ -0,0 +1,10 @@
+public class Atividade02 {
+    public static void main(String[] args) {
+        int dia = 19;
+        int mes = 9;
+        int ano = 2008;
+        System.out.println("Dia:"+dia);
+        System.out.println("Mês:"+mes);
+        System.out.println("Ano:"+ano);
+    }
+}
